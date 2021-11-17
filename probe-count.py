4256c68839cac1bdc36a40b988ee0984b948c256
root <root@PLPC009428.gft.com>
2021-11-17 19:49:06 +0100

    add probe_count script

---
+++ probe-count.py
@@ -0,0 +1,13 @@
+import pycountry
+import requests
+
+probe_count_dict = {}
+
+for country in pycountry.countries:
+    url = f"https://atlas.ripe.net:443/api/v2/probes/?country_code={country.alpha_2}"
+    with requests.get(url) as response:
+        response = response.json()
+    probe_count_dict[country.name] = [response["count"]]
+
+sort_top_10 = { country_name: probe_count for country_name, probe_count in sorted(probe_count_dict.items(), key= lambda item: item[1], reverse=True)[:10]}
+print ("here are the top 10 countries hosting probes: ", *sort_top_10)
