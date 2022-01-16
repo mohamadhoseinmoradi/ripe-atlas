ce5347de26605c8c3b267822e4356a4c2a68c63e
Mohamad Hosein Moradi <72094509+mohamadhoseinmoradi@users.noreply.github.com>
2022-01-16 12:17:18 +0100

    Update probe-count.py

--- probe-count.py
+++ probe-count.py
@@ -7,7 +7,7 @@ for country in pycountry.countries:
     url = f"https://atlas.ripe.net:443/api/v2/probes/?country_code={country.alpha_2}"
     with requests.get(url) as response:
         response = response.json()
-    probe_count_dict[country.name] = [response["count"]]
+    probe_count_dict[country.name] = response["count"]
 
 sort_top_10 = sorted(probe_count_dict.items(), key= lambda item: item[1], reverse=True)[:10]
 print ("here are the top 10 countries hosting probes: ", sort_top_10)                                                                                        
